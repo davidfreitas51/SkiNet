879e3d5430b948e1d643ef3635f3e882eda11393
David Freitas <davidfreitas51@proton.me>
2024-09-11 02:45:37 -0300

    routing and product details

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
-  declarations: [ShopComponent, ProductItemComponent],
+  declarations: [ShopComponent, ProductItemComponent, ProductDetailsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports:[
-    ShopComponent
+    ShopComponent,
   ]
 })
 export class ShopModule { }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -35,6 +35,10 @@ export class ShopService {
     return this.http.get<IProduct[]>(this.baseUrl + 'products', { params });
   }   
 
+  getProduct(id: number){
+    return this.http.get<IProduct>(this.baseUrl + 'products/' + id)
+  }
+
   getBrands(){
     return this.http.get<IBrand[]>(this.baseUrl + 'products/brands')
   }
